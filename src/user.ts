7a5f14c8e6994e20494b6e097dc7d2ff57aefa31
ShaunakMishra25 <shaunakmishra374@gmail.com>
2025-07-06 11:14:11 +0530

    style(test): fix formatting and use safe dummy password to pass Sonar

--- src/user.ts
+++ src/user.ts
@@ -1,6 +1,6 @@
-import axios from 'axios';
-import qs from 'qs';
-import { BACKEND_DOMAINS, BackendType } from './consts';
+import axios from "axios";
+import qs from "qs";
+import { BACKEND_DOMAINS, BackendType } from "./consts";
 
 const BASE_URL = `https://${BACKEND_DOMAINS[BackendType.OFF]}`;
 
@@ -16,8 +16,8 @@ export async function createUser(params: CreateUserParams): Promise<any> {
     user_id: params.username,
     password: params.password,
     email: params.email,
-    comment: params.comment || '',
-    process: 'registration'
+    comment: params.comment || "",
+    process: "registration",
   };
 
   try {
@@ -25,12 +25,14 @@ export async function createUser(params: CreateUserParams): Promise<any> {
       `${BASE_URL}/cgi/user.pl`,
       qs.stringify(payload),
       {
-        headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
-      }
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      },
     );
 
     return response.data;
   } catch (error: any) {
-    throw new Error(`Error creating user: ${error.response?.data?.error || error.message}`);
+    throw new Error(
+      `Error creating user: ${error.response?.data?.error || error.message}`,
+    );
   }
 }
